8384ca2b1f54feddb6220715ccbca33879bfc982
Abdulazeez Abdullah <64033089+AbdulazeezAbdullah@users.noreply.github.com>
2022-02-25 18:59:41 +0100

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pickle
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
-from sklearn.ensemble import RandomForestRegressor
+
 
 def load_model():
     with open('Random_Forest_Model.pkl', 'rb') as file:
