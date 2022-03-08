bf17e2a6775e4ad50a544ba22043c66c331a6a3a
Abdulazeez Abdullah <64033089+AbdulazeezAbdullah@users.noreply.github.com>
2022-03-08 17:50:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import streamlit as st
 from predict_page import show_predict_page
 from explore_page import show_explore_page
 
+st.markdown(""" <style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style> """, unsafe_allow_html=True)
+
 logo = st.sidebar.image('Stack Overflow.png')
 page = st.sidebar.selectbox("Explore or Predict", ("Predict", "Explore"))
 
